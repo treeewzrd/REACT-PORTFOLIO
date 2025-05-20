516cb729fc972ac347bace8a91282375e8643f49
treewzrd <treewzrd@gmail.com>
2025-05-19 18:03:17 -0700

    updated footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,15 +4,15 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/treeewzrd"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/treeewzrd"
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
+      link: "https://stackoverflow.com/treeewzrd"
     }
   ]
 
